a461ee068e296628296d37406af4dda6e27ccbf4
ThBrokenToaster <ruch.jack@yahoo.com>
2018-01-13 19:03:16 -0500

    Adds everything
    yeet

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public Collider[] attackHitbox;
+
+	private void attack() {
+
+    }
+}
