6b0f1df62e741f807724e385923b51befb5e3549
Alexander Shearer <sheareraws@gmail.com>
2018-10-08 22:47:47 -0400

    Adds unlock event, and unlock NPC

---
+++ Assets/Scripts/Events/UnlockEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UnlockEvent : EventAbstract {
+
+	public string title;
+	public string unlockName;
+	public bool apply = true;
+	private EventSequence seq;
+
+	override public void BeginEvent(EventSequence callback) {
+		seq = callback;
+		PlayerController.instance.unlock.AddUnlock(title, apply);
+		HUDController.instance.unlock.ShowUnlockText(unlockName, this);
+	}
+
+	override public void EndEvent() {
+		seq.NextEvent();
+	}
+}
---
+++ Assets/Scripts/HUD/UnlockHUDController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UnlockHUDController : MonoBehaviour {
+
+	private UnlockEvent unlockEvent;
+	private HUDController hud;
+	public TextMeshProUGUI unlockText;
+
+	[TextArea] public string text;
+
+	void Awake() {
+		hud = GetComponent<HUDController>();
+	}
+
+	public void ShowUnlockText(string title, UnlockEvent callback) {
+		unlockEvent = callback;
+		unlockText.text = string.Format(text, title);
+		hud.animator.SetTrigger("Unlock");
+	}
+
+	public void PostUnlockText() {
+		unlockEvent.EndEvent();
+	}
+}
