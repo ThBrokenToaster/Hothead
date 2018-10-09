0cac867c521b1fcffc55ff706a7740f69f5540a4
Alexander Shearer <sheareraws@gmail.com>
2018-10-08 21:52:30 -0400

    adds unlockable engine, with melee combo powerup

--- Assets/Scripts/Player/PlayerMelee.cs
+++ Assets/Scripts/Player/PlayerMelee.cs
@@ -11,23 +11,39 @@ public class PlayerMelee : MonoBehaviour {
 
 	// Attacking
     public Collider2D[] attackHitboxes;
-    private bool meleeButtonDown;
+
+    // Combo logistics
+    private int comboIndex = 0;
+    public float comboTimeout = 1f;
+    private float timeSinceLastAttack = 0f;
+    private bool comboActive = false;
 
 	void Awake() {
 		player = GetComponent<PlayerController>();
 	}
-	
-    // single frame inputs
-    void Update() {
-        if (!meleeButtonDown)
-            meleeButtonDown = Input.GetButtonDown("Melee");
-    }
 
 	public void MeleeUpdate() {
+        if (comboActive) {
+            timeSinceLastAttack += Time.deltaTime;
+            if (timeSinceLastAttack > comboTimeout) {
+                 comboActive = false;
+                 comboIndex = 0;
+            }
+        }
 		// Melee attack
-        if (meleeButtonDown && player.grounded && player.state != PlayerController.State.melee) {
-            meleeButtonDown = false;
-            player.animator.SetTrigger("melee");
+        if (Input.GetButtonDown("Melee") && player.grounded && player.state != PlayerController.State.melee) {
+            timeSinceLastAttack = 0f;
+            comboActive = true;
+            comboIndex++;
+
+            player.state = PlayerController.State.melee;
+
+            if (comboIndex == 3 && player.unlock.Has("meleeCombo")) {
+                player.animator.SetTrigger("meleeKick");
+                comboIndex = 0;
+            } else {
+                player.animator.SetTrigger("meleePunch");
+            }
         }
 	}
 
@@ -36,18 +52,22 @@ public class PlayerMelee : MonoBehaviour {
      * Buildup, Contact, Recovery
      */
     public void EnterMelee() {
+        // Player is locked down, animation starts
         player.state = PlayerController.State.melee;
     }
 
     public void EnableMeleeHitbox(string hitboxName) {
+        // Melee hitbox is enabled
         transform.Find(hitboxName).GetComponent<PlayerMeleeCollider>().SetEnabled(true);
     }
 
     public void DisableMeleeHitbox(string hitboxName) {
+        // Melee hitbox is disabled
         transform.Find(hitboxName).GetComponent<PlayerMeleeCollider>().SetEnabled(false);
     }
     
     public void ExitMelee() {
+        // Player is released, allowed to move
         player.state = PlayerController.State.idle;
     }
 }
---
+++ Assets/Scripts/Player/PlayerUnlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+/*
+ * Subcomponent of PlayerController managing unlockables
+ */
+public class PlayerUnlock : MonoBehaviour {
+
+	private PlayerController player;
+	private List<Unlock> unlocks = new List<Unlock>();
+
+	void Awake() {
+        player = GetComponent<PlayerController>();
+    }
+	
+	public bool Has(string title) {
+		return unlocks.Any(u => u.enabled && u.title == title);
+	}
+
+	public bool Contains(string title) {
+		return unlocks.Any(u => u.title == title);
+	}
+
+	public void AddUnlock(string title) {
+		AddUnlock(new Unlock(title, true));
+	}
+
+	public void AddUnlock(string title, bool enabled) {
+		AddUnlock(new Unlock(title, enabled));
+	}
+
+	public void AddUnlock(Unlock u) {
+		if (!Contains(u.title)) {
+			unlocks.Add(u);
+		}
+	}
+
+	public void SetUnlockEnable(string title, bool enabled) {
+		unlocks.Where(u => u.title == title).First().enabled = enabled;
+	}
+
+}
